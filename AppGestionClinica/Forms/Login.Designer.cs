f57a54560d1ff7d6cfd89f012e30cccc0116bed8
LordCasta <castanocastano6@gmail.com>
2025-05-18 21:53:05 -0500

    Se añade form de Login

---
+++ AppGestionClinica/Forms/Login.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AppGestionClinica.Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            label3 = new Label();
+            txtPassword = new TextBox();
+            btnIngresar = new Button();
+            lblError = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Emoji", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(340, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 36);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calisto MT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(187, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(259, 126);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(241, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calisto MT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(162, 185);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Contraseña";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(259, 182);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(241, 23);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(320, 250);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(123, 30);
+            btnIngresar.TabIndex = 5;
+            btnIngresar.Text = "INGRESAR";
+            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
+            // lblError
+            // 
+            lblError.AutoSize = true;
+            lblError.Font = new Font("Calibri", 11.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblError.Location = new Point(187, 314);
+            lblError.Name = "lblError";
+            lblError.Size = new Size(38, 18);
+            lblError.TabIndex = 6;
+            lblError.Text = "Error";
+            lblError.Visible = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblError);
+            Controls.Add(btnIngresar);
+            Controls.Add(txtPassword);
+            Controls.Add(label3);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private Label label3;
+        private TextBox txtPassword;
+        private Button btnIngresar;
+        private Label lblError;
+    }
+}
---
+++ AppGestionClinica/Forms/Login.cs
@@ -0,0 +1,36 @@
+﻿using AppGestionClinica.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace AppGestionClinica.Forms
+{
+    public partial class Login : Form
+    {
+        private readonly UsuarioRepository _usuarioRepo = new UsuarioRepository();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string user = txtUsuario.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))
+            {
+                lblError.Text = "Debe ingresar usuario y contraseña";
+                lblError.Visible = true;
+                return;
+            }
+        }
+    }
+}
