7b7801da79b55e0e33a085aed9459720302df0f3
LordCasta <castanocastano6@gmail.com>
2025-05-18 20:40:50 -0500

    Se añade el Repository para Pago

--- AppGestionClinica/Entities/Pago.cs
+++ AppGestionClinica/Entities/Pago.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace AppGestionClinica.Entities
 {
-    public class Pagos
+    public class Pago
     {
         public int PagoID { get; set; }
-        public int TratamiendoID { get; set; }
+        public int TratamientoID { get; set; }
         public decimal Monto { get; set; }
         public DateTime FechaPago { get; set; }
         public string MetodoPago { get; set; } // Efectivo, Tarjeta, Transferencia
---
+++ AppGestionClinica/Repository/PagoRepository.cs
@@ -0,0 +1,74 @@
+﻿using AppGestionClinica.Data;
+using AppGestionClinica.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestionClinica.Repository
+{
+    public class PagoRepository
+    {
+        private readonly SqlConnection _conn;
+        public PagoRepository() => _conn = Database.GetConnection();
+
+        public void Agregar(Pago p)
+        {
+            const string sql = @"
+                INSERT INTO Pagos (TratamientoID,FechaPago,Monto,MetodoPago)
+                VALUES (@TratamientoID,GETDATE(),@Monto,@MetodoPago)";
+            using var cmd = new SqlCommand(sql, _conn);
+            cmd.Parameters.AddWithValue("@TratamientoID", p.TratamientoID);
+            cmd.Parameters.AddWithValue("@Monto", p.Monto);
+            cmd.Parameters.AddWithValue("@MetodoPago", p.MetodoPago);
+            cmd.ExecuteNonQuery();
+        }
+
+        public List<Pago> ObtenerTodos()
+        {
+            var list = new List<Pago>();
+            const string sql = "SELECT * FROM Pagos";
+            using var cmd = new SqlCommand(sql, _conn);
+            using var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                list.Add(new Pago
+                {
+                    PagoID = (int)rdr["PagoID"],
+                    TratamientoID = (int)rdr["TratamientoID"],
+                    FechaPago = (DateTime)rdr["FechaPago"],
+                    Monto = (decimal)rdr["Monto"],
+                    MetodoPago = (string)rdr["MetodoPago"]
+                });
+            }
+            return list;
+        }
+
+        public Pago ObtenerPorId(int id)
+        {
+            const string sql = "SELECT * FROM Pagos WHERE PagoID=@id";
+            using var cmd = new SqlCommand(sql, _conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            using var rdr = cmd.ExecuteReader();
+            if (rdr.Read()) return new Pago
+            {
+                PagoID = (int)rdr["PagoID"],
+                TratamientoID = (int)rdr["TratamientoID"],
+                FechaPago = (DateTime)rdr["FechaPago"],
+                Monto = (decimal)rdr["Monto"],
+                MetodoPago = (string)rdr["MetodoPago"]
+            };
+            return null;
+        }
+
+        public void Eliminar(int id)
+        {
+            const string sql = "DELETE FROM Pagos WHERE PagoID=@id";
+            using var cmd = new SqlCommand(sql, _conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
