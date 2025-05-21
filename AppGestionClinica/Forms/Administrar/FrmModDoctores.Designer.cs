2cd331fb8bd957cbb0b25ef53d9c137bcc4ee1bb
LordCasta <castanocastano6@gmail.com>
2025-05-21 00:38:07 -0500

    Se termina CRUD Doctores

--- AppGestionClinica/Forms/Administrar/FrmModDoctores.Designer.cs
+++ AppGestionClinica/Forms/Administrar/FrmModDoctores.Designer.cs
@@ -31,9 +31,16 @@
             components = new System.ComponentModel.Container();
             label1 = new Label();
             btnCrearDoctor = new Button();
-            dataGridView1 = new DataGridView();
+            dgvDoctores = new DataGridView();
             doctorRepositoryBindingSource = new BindingSource(components);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            label2 = new Label();
+            label3 = new Label();
+            txtNombre = new TextBox();
+            txt1 = new Label();
+            cmbEspecializacion = new ComboBox();
+            label4 = new Label();
+            txtTelefono = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvDoctores).BeginInit();
             ((System.ComponentModel.ISupportInitialize)doctorRepositoryBindingSource).BeginInit();
             SuspendLayout();
             // 
@@ -49,38 +56,105 @@
             // 
             // btnCrearDoctor
             // 
-            btnCrearDoctor.Location = new Point(45, 171);
+            btnCrearDoctor.Location = new Point(118, 303);
             btnCrearDoctor.Name = "btnCrearDoctor";
             btnCrearDoctor.Size = new Size(107, 31);
             btnCrearDoctor.TabIndex = 8;
             btnCrearDoctor.Text = "Crear";
             btnCrearDoctor.UseVisualStyleBackColor = true;
+            btnCrearDoctor.Click += btnCrearDoctor_Click;
             // 
-            // dataGridView1
+            // dgvDoctores
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(216, 78);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(609, 447);
-            dataGridView1.TabIndex = 9;
+            dgvDoctores.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDoctores.Location = new Point(476, 99);
+            dgvDoctores.Name = "dgvDoctores";
+            dgvDoctores.Size = new Size(535, 572);
+            dgvDoctores.TabIndex = 9;
+            dgvDoctores.CellClick += dgvDoctores_CellClick;
             // 
             // doctorRepositoryBindingSource
             // 
             doctorRepositoryBindingSource.DataSource = typeof(Repository.DoctorRepository);
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(138, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 17);
+            label2.TabIndex = 10;
+            label2.Text = "CREAR";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 160);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 17);
+            label3.TabIndex = 11;
+            label3.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(118, 157);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(215, 25);
+            txtNombre.TabIndex = 12;
+            // 
+            // txt1
+            // 
+            txt1.AutoSize = true;
+            txt1.Location = new Point(12, 208);
+            txt1.Name = "txt1";
+            txt1.Size = new Size(100, 17);
+            txt1.TabIndex = 13;
+            txt1.Text = "Especialización";
+            // 
+            // cmbEspecializacion
+            // 
+            cmbEspecializacion.FormattingEnabled = true;
+            cmbEspecializacion.Location = new Point(118, 200);
+            cmbEspecializacion.Name = "cmbEspecializacion";
+            cmbEspecializacion.Size = new Size(215, 25);
+            cmbEspecializacion.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 253);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 17);
+            label4.TabIndex = 15;
+            label4.Text = "Telefono";
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(118, 245);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(215, 25);
+            txtTelefono.TabIndex = 16;
+            // 
             // FrmModDoctores
             // 
             AutoScaleDimensions = new SizeF(8F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 548);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(1048, 749);
+            Controls.Add(txtTelefono);
+            Controls.Add(label4);
+            Controls.Add(cmbEspecializacion);
+            Controls.Add(txt1);
+            Controls.Add(txtNombre);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dgvDoctores);
             Controls.Add(btnCrearDoctor);
             Controls.Add(label1);
             Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "FrmModDoctores";
             Text = "FrmModDoctores";
             Load += FrmModDoctores_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDoctores).EndInit();
             ((System.ComponentModel.ISupportInitialize)doctorRepositoryBindingSource).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -90,7 +164,14 @@
 
         private Label label1;
         private Button btnCrearDoctor;
-        private DataGridView dataGridView1;
+        private DataGridView dgvDoctores;
         private BindingSource doctorRepositoryBindingSource;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNombre;
+        private Label txt1;
+        private ComboBox cmbEspecializacion;
+        private Label label4;
+        private TextBox txtTelefono;
     }
 }
--- AppGestionClinica/Forms/Administrar/FrmModDoctores.cs
+++ AppGestionClinica/Forms/Administrar/FrmModDoctores.cs
@@ -16,6 +16,7 @@ namespace AppGestionClinica.Forms.Administrar
     {
         DoctorRepository repo = new DoctorRepository();
         BindingList<Doctor> listaDoctores;
+        private int? doctorSeleccionadoId = null;
         public FrmModDoctores()
         {
             InitializeComponent();
@@ -23,11 +24,109 @@ namespace AppGestionClinica.Forms.Administrar
 
         private void FrmModDoctores_Load(object sender, EventArgs e)
         {
+            CargarDoctores();
+            #region
+            // Cargar especializaciones en el ComboBox
+            cmbEspecializacion.Items.Add("General");
+            cmbEspecializacion.Items.Add("Cardiología");
+            cmbEspecializacion.Items.Add("Dermatología");
+            cmbEspecializacion.Items.Add("Ginecología");
+            cmbEspecializacion.Items.Add("Pediatría");
+            cmbEspecializacion.Items.Add("Psiquiatría");
+            cmbEspecializacion.Items.Add("Traumatología");
+            #endregion
+        }
+
+        private void CargarDoctores()
+        {
             listaDoctores = new BindingList<Doctor>(repo.ObtenerTodos());
-            dataGridView1.DataSource = listaDoctores;
+            dgvDoctores.DataSource = listaDoctores;
 
             // Opcional: ocultar la columna ID si no quieres editarla
-            dataGridView1.Columns["DoctorID"].ReadOnly = true;
+            dgvDoctores.Columns["DoctorID"].ReadOnly = true;
+
+            if (!dgvDoctores.Columns.Contains("Editar"))
+            {
+                var btnEditar = new DataGridViewButtonColumn();
+                btnEditar.HeaderText = "Editar";
+                btnEditar.Text = "Editar";
+                btnEditar.UseColumnTextForButtonValue = true;
+                dgvDoctores.Columns.Add(btnEditar);
+
+                var btnEliminar = new DataGridViewButtonColumn();
+                btnEliminar.HeaderText = "Eliminar";
+                btnEliminar.Text = "Eliminar";
+                btnEliminar.UseColumnTextForButtonValue = true;
+                dgvDoctores.Columns.Add(btnEliminar);
+            }
+
+
+
+        }
+
+        private void btnCrearDoctor_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text) || string.IsNullOrWhiteSpace(cmbEspecializacion.Text))
+            {
+                MessageBox.Show("Por favor complete todos los campos.");
+                return;
+            }
+
+            var doctor = new Doctor
+            {
+                Nombre = txtNombre.Text,
+                Especializacion = cmbEspecializacion.Text,
+                Telefono = txtTelefono.Text
+            };
+
+            repo.Agregar(doctor);
+            MessageBox.Show("Doctor agregado.");
+            LimpiarFormulario();
+            CargarDoctores();
+        }
+
+        private void dgvDoctores_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            var fila = dgvDoctores.Rows[e.RowIndex];
+            int doctorId = Convert.ToInt32(fila.Cells["DoctorID"].Value);
+
+            if (dgvDoctores.Columns[e.ColumnIndex].HeaderText == "Editar")
+            {
+                string nuevoNombre = fila.Cells["Nombre"].Value.ToString();
+                string nuevaEsp = fila.Cells["Especializacion"].Value.ToString();
+                string nuevoTel = fila.Cells["Telefono"].Value.ToString();
+
+                var doctor = new Doctor
+                {
+                    DoctorID = doctorId,
+                    Nombre = nuevoNombre,
+                    Especializacion = nuevaEsp,
+                    Telefono = nuevoTel
+                };
+
+                repo.Actualizar(doctor);
+                MessageBox.Show("Doctor actualizado.");
+            }
+            else if (dgvDoctores.Columns[e.ColumnIndex].HeaderText == "Eliminar")
+            {
+                var confirmar = MessageBox.Show("¿Eliminar este doctor?", "Confirmar", MessageBoxButtons.YesNo);
+                if (confirmar == DialogResult.Yes)
+                {
+                    repo.Eliminar(doctorId);
+                    CargarDoctores();
+                    MessageBox.Show("Doctor eliminado.");
+                }
+            }
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtNombre.Text = "";
+            txtTelefono.Text = "";
+            doctorSeleccionadoId = null;
+            dgvDoctores.ClearSelection();
         }
     }
 }
