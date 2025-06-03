867f9edfcdb1e9e0a51115d0157eebe4fb3daa5e
LordCasta <castanocastano6@gmail.com>
2025-06-02 22:04:05 -0500

    Se añade Patrón de diseño Unit Of Work
    Se añade clase para implementar el patrón de diseño UOF, usando los Repository de cada Entidad, centralizando el trabajo

---
+++ AppGestionClinica/Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using AppGestionClinica.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestionClinica.Data
+{
+    public class UnitOfWork
+    {
+        public PacienteRepository Pacientes { get; }
+        public DoctorRepository Doctores { get; }
+        public UsuarioRepository Usuarios { get; }
+        public CitaRepository Citas { get; }
+        public TratamientoRepository Tratamientos { get; }
+        public PagoRepository Pagos { get; }
+
+        public UnitOfWork()
+        {
+            Pacientes = new PacienteRepository();
+            Doctores = new DoctorRepository();
+            Usuarios = new UsuarioRepository();
+            Citas = new CitaRepository();
+            Tratamientos = new TratamientoRepository();
+            Pagos = new PagoRepository();
+        }
+    }
+}
